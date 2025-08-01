a483abebc70a8c0d70afcd58edd53cccef0ba85d
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-08-01 22:42:48 +0300

    Adding input data for employee like upload contract, email, education, date of adding and note in AddEmployee file

--- my-dashboard/src/views/AddEmployee.vue
+++ my-dashboard/src/views/AddEmployee.vue
@@ -2,10 +2,29 @@
   <div class="p-6">
     <h2 class="text-2xl font-bold mb-4">Add new employee</h2>
     
-    <form @submit.prevent="addEmployee" class="grid grid-cols-2 gap-4">
+    <form @submit.prevent="addEmployee" class="grid grid-cols-3 gap-4">
       <div>
         <label class="block mb-1">Full name</label>
         <input v-model="employee.name" type="text" class="w-full border border-gray-300 rounded px-3 py-2" required />
+      </div>
+
+      <div>
+        <label class="block mb-1">Education</label>
+        <select v-model="employee.education" class="w-full border border-gray-300 rounded px-3 py-2" required>
+          <option>Bacholar</option>
+          <option>Master</option>
+          <option>Prof</option>
+        </select>
+      </div>
+
+      <div>
+        <label class="block mb-1">Phone number</label>
+        <input v-model="employee.phoneNumber" type="number" class="w-full border border-gray-300 rounded px-3 py-2" required />
+      </div>
+
+      <div>
+        <label class="block mb-1">Email</label>
+        <input v-model="employee.email" type="email" class="w-full border border-gray-300 rounded px-3 py-2" required />
       </div>
 
       <div>
@@ -28,6 +47,16 @@
         <input v-model.number="employee.deductions" type="number" class="w-full border border-gray-300 rounded px-3 py-2" />
       </div>
 
+      <div>
+        <label class="block mb-1">Date</label>
+        <input v-model.date="employee.date" type="datetime-local" class="w-full border border-gray-300 rounded px-3 py-2" required />
+      </div>
+
+      <div>
+        <label class="block mb-1">upload contruct</label>
+        <input v-on="employee.contract" type="file" class="w-full border border-gray-300 rounded px-3 py-2" />
+      </div><br></br>
+
       <div class="col-span-2">
         <button type="submit" class="bg-orange-400 hover:bg-orange-500 text-white py-2 px-4 rounded">
           Save
@@ -38,17 +67,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, vModelSelect } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
 const employee = ref({
   name: '',
+  education: '',
+  phoneNumber: '',
+  email: '',
   department: '',
   baseSalary: 0,
   bonus: 0,
-  deductions: 0
+  deductions: 0 ,
+  date: ''
 })
 
 const addEmployee = () => {
@@ -57,6 +90,7 @@ const addEmployee = () => {
     existing.push({ ...employee.value })
     localStorage.setItem('employees', JSON.stringify(existing))
     router.push('/employees') 
+    alert('employee added seccesfully ! ')
   } catch (err) {
     console.error('Saving failed:', err)
   }
